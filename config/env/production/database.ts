9efa328422c9960ecff3c293b0999e566ebfebab
Taylor Fairbank <taylor@distributeaid.org>
2023-10-21 00:31:25 +0000

    feat(prod): setup a production env

---
+++ config/env/production/database.ts
@@ -0,0 +1,17 @@
+// from: https://docs.strapi.io/dev-docs/deployment/digitalocean-app-platform
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST'), 
+      port: env.int('DATABASE_PORT'), 
+      database: env('DATABASE_NAME'), 
+      user: env('DATABASE_USERNAME'), 
+      password: env('DATABASE_PASSWORD'),
+      ssl: {
+        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+      },
+    },
+    debug: false,
+  },
+});
---
+++ config/env/production/server.ts
@@ -0,0 +1,8 @@
+// from: https://docs.strapi.io/dev-docs/deployment/digitalocean-app-platform
+export default ({ env }) => ({
+  proxy: true,
+  url: env('APP_URL'), // Sets the public URL of the application.
+  app: { 
+    keys: env.array('APP_KEYS')
+  },
+});
