76693eb51f3273ec9864e45cf9f47068263dcd2a
Taylor Fairbank <taylor@distributeaid.org>
2023-10-22 08:07:22 +0000

    feat(prod): setup production db

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,15 +1,21 @@
 // from: https://docs.strapi.io/dev-docs/deployment/digitalocean-app-platform
+const parse = require("pg-connection-string").parse;
+
+const { host, port, database, user, password } = parse(
+  process.env.DATABASE_URL
+);
+
 export default ({ env }) => ({
-  connection: {
+  connection: {  
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'), 
-      port: env.int('DATABASE_PORT'), 
-      database: env('DATABASE_NAME'), 
-      user: env('DATABASE_USERNAME'), 
-      password: env('DATABASE_PASSWORD'),
+      host,
+      port,
+      database,
+      user,
+      password,
       ssl: {
-        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+        ca: env('DATABASE_CA'),
       },
     },
     debug: false,
