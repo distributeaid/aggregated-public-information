09ae3b639f029ea3f95857250ca24f50b508f6b6
Taylor Fairbank <taylor@distributeaid.org>
2023-10-22 07:24:49 +0000

    feat(prod): setup production media storage

--- config/plugins.ts
+++ config/plugins.ts
@@ -19,4 +19,26 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        baseUrl: env('DO_CDN_URL'),
+        rootPath: env('DO_CDN_ROOT_PATH'),
+        s3Options: {
+          accessKeyId: env('DO_SPACES_ACCESS_KEY_ID'),
+          secretAccessKey: env('DO_SPACES_ACCESS_SECRET'),
+          endpoint: env('DO_SPACES_ENDPOINT'),
+          params: {
+            Bucket: env('DO_SPACES_BUCKET'),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
