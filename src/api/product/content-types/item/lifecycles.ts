4fccf316b15ea2e72cf80b8c2089604ba9565279
Travis Almey <travisalmey@gmail.com>
2024-03-26 15:00:38 -0500

    more testing

--- src/api/product/content-types/item/lifecycles.ts
+++ src/api/product/content-types/item/lifecycles.ts
@@ -1,14 +1,34 @@
 export default {
-  async beforeCreate(event) {
+  async afterCreate(event) {
     const { data } = event.params;
-    const weight = data.weight.packageWeight;
-    const numItems = data.weight.countPerPackage;
-    const itemWtKg = weight / numItems;
-
-    event.params.data.weight.itemWeightKg = itemWtKg;
+    // strapi.log.info(`data: ${JSON.stringify(event)}`)
     strapi.log.info(`data: ${JSON.stringify(data)}`);
-    strapi.log.info(`weight: ${weight}`);
-    strapi.log.info(`numItems: ${numItems}`);
-    strapi.log.info(`itemWtKg: ${itemWtKg}`);
+
+    const weightID = data.weight.id;
+
+    // strapi.log.info(`weightID: ${weightID}`);
+
+    // const entry = await strapi.db.query('api::product.product-weight').findOne({
+    //   select: ['packageWeight', 'countPerPackage'],
+    //   where: { id: weightID }
+    // });
+
+    // strapi.log.info(`entry: ${JSON.stringify(entry)}`);
+
+    const entry = await strapi.entityService.findOne('api::product.item', data.id, {
+      populate: `*`
+    });
+
+    strapi.log.info(`entry: ${JSON.stringify(entry)}`);
+
+    // const weight = data.weight.packageWeight;
+    // const numItems = data.weight.countPerPackage;
+    // const itemWtKg = weight / numItems;
+
+    // event.params.data.weight.itemWeightKg = itemWtKg;
+    // strapi.log.info(`data: ${JSON.stringify(data)}`);
+    // strapi.log.info(`weight: ${weight}`);
+    // strapi.log.info(`numItems: ${numItems}`);
+    // strapi.log.info(`itemWtKg: ${itemWtKg}`);
   },
 };
