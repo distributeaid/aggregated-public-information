e2bac8969312656c11051911462635d7143dc319
Travis Almey <travisalmey@gmail.com>
2024-03-28 14:34:31 -0500

    First attempts at getting data from the controllers

--- src/api/product/controllers/item.ts
+++ src/api/product/controllers/item.ts
@@ -4,4 +4,28 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::product.item');
+export default factories.createCoreController('api::product.item', ({ strapi }) => ({
+    async find(ctx) {
+        const result = await super.find(ctx);
+        strapi.log.info(`result of find: ${JSON.stringify(result)}`);
+        return result;
+    },
+
+    async findOne(ctx) {
+        const result = await super.findOne(ctx);
+        strapi.log.info(`result of findOne: ${JSON.stringify(result)}`);
+        return result;
+    },
+
+    async create(ctx) {
+        const result = await super.create(ctx);
+        strapi.log.info(`result of create: ${JSON.stringify(result)}`);
+        return result;
+    },
+
+    async update(ctx) {
+        const result = await super.update(ctx);
+        strapi.log.info(`result of update: ${JSON.stringify(result)}`);
+        return result;
+    }
+}));
