7c0819cf857fe717a293250f7adb05431a2eb753
Dierdre D Dixon <d@themadcalf.com>
2024-04-04 07:55:48 +0000

    fixup! Extend content-manager plugin to override `create` and `update` methods

--- src/extensions/content-manager/strapi-server.ts
+++ src/extensions/content-manager/strapi-server.ts
@@ -1,35 +1,33 @@
 module.exports = (plugin) => {
   let originalFind = plugin.controllers["collection-types"].find;
   plugin.controllers["collection-types"].find = (ctx) => {
-    // strapi.log.info(`FIND ${JSON.stringify(ctx)}`);
-
+    strapi.log.info(`ADMIN PANEL FIND ${JSON.stringify(ctx.request)}`);
     return originalFind(ctx);
   };
 
   let originalFindOne = plugin.controllers["collection-types"].findOne;
   plugin.controllers["collection-types"].findOne = (ctx) => {
-    // strapi.log.info(`FINDONE ${JSON.stringify(ctx)}`);
+    strapi.log.info(`ADMIN PANEL FINEONE ${JSON.stringify(ctx.request.body)}`);
 
     return originalFindOne(ctx);
   };
 
   let originalCreate = plugin.controllers["collection-types"].create;
   plugin.controllers["collection-types"].create = (ctx) => {
-    strapi.log.info(`CREATE CTX BODY ${JSON.stringify(ctx.request.body)}`);
-    const { packageWeight, countPerPackage } = ctx.request.body.weight;
-    const itemWeightKg = packageWeight / countPerPackage;
-    ctx.request.body.weight.itemWeightKg = itemWeightKg;
+    strapi.log.info(`ADMIN PANEL CREATE ${JSON.stringify(ctx.request.body)}`);
+
+    if (ctx.request.body.weight) {
+      ctx.request.body.weight = calculateWeightFields(ctx.request.body.weight);
+    }
 
     return originalCreate(ctx);
   };
 
   let originalUpdate = plugin.controllers["collection-types"].update;
   plugin.controllers["collection-types"].update = (ctx) => {
-    strapi.log.info(`UPDATE CTX BODY ${JSON.stringify(ctx.request.body)}`);
-    const { packageWeight, countPerPackage } = ctx.request.body.weight;
-    const itemWeightKg = packageWeight / countPerPackage;
-    ctx.request.body.weight.itemWeightKg = itemWeightKg;
-    strapi.log.info(`NEW CTX BODY ${JSON.stringify(ctx.request.body)}`);
+    strapi.log.info(`ADMIN PANEL UPDATE ${JSON.stringify(ctx.request.body)}`);
+
+    ctx.request.body.weight = calculateWeightFields(ctx.request.body.weight);
 
     return originalUpdate(ctx);
   };
@@ -37,9 +35,10 @@ module.exports = (plugin) => {
   return plugin;
 };
 
-// function calculateWeightValues(weightData) {
-//   const { packageWeight, countPerPackage } = ctx.request.body.weight;
-//   const itemWeightKg = packageWeight / countPerPackage;
-//   ctx.request.body.weight.itemWeightKg = itemWeightKg;
-//   strapi.log.info(`NEW CTX BODY ${JSON.stringify(ctx.request.body)}`);
-// }
+function calculateWeightFields(weightData) {
+  const { packageWeight, countPerPackage } = weightData;
+  const itemWeightKg = packageWeight / countPerPackage;
+  const countPerKg = countPerPackage / itemWeightKg;
+
+  return { ...weightData, itemWeightKg, countPerKg };
+}
