275e49d531fc5986aa69c2548ee56b688b78f755
Laura Z. <llzerr@hotmail.com>
2024-05-24 17:23:28 +0000

    feat:add need collection type

---
+++ src/api/needs-assessment/controllers/need.ts
@@ -0,0 +1,7 @@
+/**
+ * need controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::needs-assessment.need');
---
+++ src/api/needs-assessment/routes/need.ts
@@ -0,0 +1,7 @@
+/**
+ * need router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::needs-assessment.need');
