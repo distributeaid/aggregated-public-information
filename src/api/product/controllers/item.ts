9743de8a1c5f248d91faeeb1ad69d97c3dcf583d
D Dixon <d@themadcalf.com>
2024-06-05 15:26:52 -0700

    feat: customize `product.item` controller to process data entry calculations
    - Overwrites the CREATE and UPDATE methods to trigger custom
      calculations when creating and updating records with the API

--- src/api/product/controllers/item.ts
+++ src/api/product/controllers/item.ts
@@ -2,6 +2,31 @@
  * item controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
+import { processContentType } from "../../../functions/content-types";
 
-export default factories.createCoreController('api::product.item');
+export default factories.createCoreController(
+  "api::product.item",
+
+  ({ strapi }) => ({
+    async create(ctx) {
+      ctx.request.body.data = processContentType(
+        ctx.request.path,
+        ctx.request.body.data
+      );
+
+      const result = await super.create(ctx);
+      return result;
+    },
+
+    async update(ctx) {
+      ctx.request.body.data = processContentType(
+        ctx.request.path,
+        ctx.request.body.data
+      );
+
+      const result = await super.update(ctx);
+      return result;
+    },
+  })
+);
