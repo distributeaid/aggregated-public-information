4e81319264625c5ca75b42e4a7a35cc59b7458f2
D Dixon <d@themadcalf.com>
2024-06-05 15:03:56 -0700

    feat: add content type processing functions for data entry calculations

---
+++ src/functions/content-types/index.ts
@@ -0,0 +1,34 @@
+import { processProductItem } from "./product/item";
+
+export function processContentType(path, data) {
+  /*
+   Content-manager paths look like this:
+    http://host/admin/content-manager/collection-types/api::product.item/15
+
+   API paths look like this (note the use of the plural version here!)
+    http://host/api/items/15 )
+
+   So need to map both options to the appropriate handler
+  */
+  const contentTypeHandlers = {
+    "product.item": processProductItem,
+    items: processProductItem,
+  };
+
+  const contentType = detectContentType(path);
+
+  const handler = contentTypeHandlers[contentType];
+  if (handler) {
+    return handler(data);
+  }
+}
+
+function detectContentType(path) {
+  if (path.includes("content-manager/collection-types/api::")) {
+    return path.split("api::")[1].split("/")[0];
+  }
+
+  if (path.includes("/api/")) {
+    return path.split("/api/")[1].split("/")[0];
+  }
+}
