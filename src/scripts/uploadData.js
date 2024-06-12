4c8da212c4178d37502e21934b297722aec44edc
Laura Z. <llzerr@hotmail.com>
2024-06-12 21:35:24 +0000

    feat:data processing for place prior to data transfer

---
+++ src/scripts/uploadData.js
@@ -0,0 +1,33 @@
+import 'dotenv/config'
+import fetch from 'node-fetch'
+import { readFileSync } from 'fs'
+
+const strapiURL = 'https://1337-distributea-aggregatedp-hvms4i4hj5d.ws-us114.gitpod.io/needs';
+const apiKey = process.env.API_TOKEN_SALT;
+
+const jsonData = readFileSync("./needs-data.json", 'utf8');
+const data = JSON.parse(jsonData);
+
+const places = data.map((need) => {
+    return need.place
+}) 
+
+// Create regions
+// ------------------------------------
+const regions = places.map((place) => {
+    return place.region
+})
+
+const uniqueRegions = Array.from(new Set(regions))
+
+
+
+
+
+// Create subregions
+// ------------------------------------
+const subregions = places.map((place) => {
+    return place.subregion
+})
+
+const uniqueSubregions = Array.from(new Set(subregions))
