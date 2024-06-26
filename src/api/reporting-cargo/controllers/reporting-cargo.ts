0ecfe0388eb17f9a6450c4fd2d26436ccf7faf7a
Ivy Lou <qianlou.ivy@gmail.com>
2024-06-26 12:38:34 +0000

    Created the cargo model on Strapi, created variables until the calculations. Need to add all relavent variables to complete the model.

---
+++ src/api/reporting-cargo/controllers/reporting-cargo.ts
@@ -0,0 +1,7 @@
+/**
+ * reporting-cargo controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::reporting-cargo.reporting-cargo');
---
+++ src/api/reporting-cargo/routes/reporting-cargo.ts
@@ -0,0 +1,7 @@
+/**
+ * reporting-cargo router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::reporting-cargo.reporting-cargo');
