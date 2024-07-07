a34fe77fbc05938ead27197231bbe454f16fc1c1
Ivy Lou <qianlou.ivy@gmail.com>
2024-07-07 04:01:46 +0000

    Added Functions to the Group model

--- src/api/group/controllers/group.ts
+++ src/api/group/controllers/group.ts
@@ -3,5 +3,30 @@
  */
 
 import { factories } from '@strapi/strapi'
+import { processContentType } from "../../../functions/content-types";
 
-export default factories.createCoreController('api::group.group');
+export default factories.createCoreController(
+    'api::group.group',
+
+    ({ strapi }) => ({
+        async create(ctx) {
+          ctx.request.body.data = processContentType(
+            ctx.request.path,
+            ctx.request.body.data
+          );
+    
+          const result = await super.create(ctx);
+          return result;
+        },
+    
+        async update(ctx) {
+          ctx.request.body.data = processContentType(
+            ctx.request.path,
+            ctx.request.body.data
+          );
+    
+          const result = await super.update(ctx);
+          return result;
+        },
+      })
+);
