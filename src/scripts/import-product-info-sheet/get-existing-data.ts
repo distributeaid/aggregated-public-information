b9b99512635143f0780dd2ec4d9d6b17283adbf4
Taylor Fairbank <taylor@distributeaid.org>
2024-08-03 17:05:59 +0000

    feat: use a promise-chaining workflow for the item upload process

--- src/scripts/import-product-info-sheet/get-existing-data.ts
+++ src/scripts/import-product-info-sheet/get-existing-data.ts
@@ -1,5 +1,3 @@
-import qs from 'qs'
-
 import { STRAPI_ENV } from "../strapi-env"
 
 export type NameToIdMap = {
@@ -34,68 +32,4 @@ export async function getCategories() {
   )
 
   return existingCategories
-}
-
-/* Does Item Exist
- * ----------------------------------------------------- */
-export async function doesItemExist(item): Promise<boolean> {
-  const query = qs.stringify({
-      filters: {
-        name: {
-          $eq: item.name,
-        },
-        age_gender: {
-          $eq: item.age_gender,
-        },
-        size_style: {
-          $eq: item.size_style
-        }
-      },
-      fields: ['id'],
-  })
-
-  return fetch(`${STRAPI_ENV.URL}/items?${query}`, {
-    method: "GET",
-    headers: {
-        "Content-Type": "application/json",
-        'Authorization': `Bearer ${STRAPI_ENV.KEY}`
-    },
-  })
-    .then(async (response) => {
-      if (response.ok) {
-        return response.json()
-      } else {
-        throw new Error(`  - failed to get item (${item.name} // ${item.age_gender} // ${item.size_style}). HttpError: ${response.status} - ${response.statusText}`)
-      }
-    })
-    .then((result) => {
-      return result.data.length > 0
-    })   
-}
-
-export async function getItems() {
-  console.log("    - getting existing items")
-
-  let response = await fetch(`${STRAPI_ENV.URL}/items`, {
-    method: "GET",
-    headers: {
-        "Content-Type": "application/json",
-        'Authorization': `Bearer ${STRAPI_ENV.KEY}`
-    },
-  });
-
-  if (!response.ok) {
-    console.log(response)
-    throw new Error(`HTTP error! Status: ${response.status}`);
-  }
-
-  const responseJson = await response.json()
-  const existingItems = responseJson.data.reduce(
-    (items: Set<string>, item) => {
-      return items.add(item.attributes.name)
-    },
-    new Set<string>()
-  )
-
-  return existingItems
 }
