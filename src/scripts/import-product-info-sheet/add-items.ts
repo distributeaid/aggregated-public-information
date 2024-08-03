89d645ccf2c26c00b54be10cd62f89a6e5c6dd8d
Taylor Fairbank <taylor@distributeaid.org>
2024-08-03 17:05:59 +0000

    feat: use a promise-chaining workflow for the item upload process

--- src/scripts/import-product-info-sheet/add-items.ts
+++ src/scripts/import-product-info-sheet/add-items.ts
@@ -1,6 +1,7 @@
-import { toTitleCase } from "../helpers"
+import qs from 'qs'
 import { STRAPI_ENV } from "../strapi-env"
-import { doesItemExist, getCategories, getItems } from "./get-existing-data"
+import { toTitleCase } from "../helpers"
+import { getCategories } from "./get-existing-data"
 import type { NameToIdMap } from "./get-existing-data"
 
 /* Categories
@@ -11,17 +12,26 @@ import type { NameToIdMap } from "./get-existing-data"
 export default async function addItems(products) {
   console.log("Adding items from Product Info sheet...")
 
-  const existingCategories = await getCategories()
-  const items = await parseItems(products, existingCategories)
-  const validItems = validateItems(products)
-  const results = await uploadNewItems(items)
+  const categories = await getCategories()
+  const results = await Promise.allSettled(
+    products.map((product) => {
+      return parseItem(product, categories)
+        .then((value) => {
+          return value
+        })
+        .then(getItem)
+        .then(uploadItem)
+    })
+  )
 
-  console.log(results)
+  results.forEach((result) => {
+    console.log(result)
+  })
 
   console.log("Adding items completed!")
 }
 
-/* Parse Items
+/* Parse Item
  * ------------------------------------------------------ */
 function stripAndParseInt(numberString: string): number {
   return parseInt(
@@ -35,193 +45,434 @@ function stripAndParseFloat(numberString: string): number {
   )
 }
 
-function parseItems(products, existingCategories: NameToIdMap) {
-  console.log("    - parsing items")
+function parseItem(
+  origItem,
+  categories: NameToIdMap
+): Promise<{item: any, origItem: any, logs: string[]}> {
+  const logs = [
+    `Log: Parsing item "${origItem.name} // ${origItem.age_gender} // ${origItem.size_style}".`
+  ]
 
-  const items = products.map((product) => {
-    // make some adjustments to our CSV input data to prep it for upload
-    const item = {
-      category: existingCategories[toTitleCase(product.category.name)],
-      name: toTitleCase(product.name),
-      age_gender: toTitleCase(product.age_gender),
-      size_style: product.size_style,
-      packSize: stripAndParseInt(product.packSize),
-      uuid: undefined, // TODO: add uuid support in the model
-
-      needsMet: parseNeedsMet(product),
-      secondHand: parseSecondHand(product),
-      value: parseValue(product),
-      weight: parseWeight(product),
-      volume: parseVolume(product)
+  return new Promise<{item: any, origItem: any, logs: string[]}>((resolve, reject) => {
+    if (origItem.name === "") {
+      reject({
+        item: {},
+        origItem,
+        logs: [
+          ...logs,
+          "Error: Item is missing a name. Skipping..."
+        ]
+      })
     }
 
-    return item
+    resolve({
+      item: {
+        name: toTitleCase(origItem.name),
+        age_gender: toTitleCase(origItem.age_gender),
+        size_style: origItem.size_style,
+        packSize: stripAndParseInt(origItem.packSize) || 1, // default to 1
+        uuid: undefined, // TODO: add uuid support in the model
+      },
+      origItem,
+      logs
+    })
   })
-
-  return items
+    .then((results) => {
+      return parseCategory(results, categories)
+    })
+    .then(parseNeedsMet)
+    .then(parseSecondHand)
+    .then(parseValue)
+    .then(parseWeight)
+    .then(parseVolume)
+    .then(({item, logs}) => {
+      return {
+        item,
+        origItem,
+        logs: [
+          ...logs,
+          "Log: Successfully parsed item."
+        ]
+      }
+    })
 }
 
-function parseNeedsMet(product) {
-  const needsMet = product.needsMet
+function parseCategory(
+  {item, origItem, logs}: {item: any, origItem: any, logs: string[]},
+  categories: NameToIdMap
+) {
+  if (origItem.category.name === "") {
+    throw {
+      item,
+      origItem,
+      logs: [
+        ...logs,
+        "Error: Category is missing a name.  Skipping..."
+      ]
+    }
+  }
 
+  const categoryId = categories[origItem.category.name]
+  if (categoryId === undefined) {
+    throw {
+      item,
+      origItem,
+      logs: [
+        ...logs,
+        "Error: Category doesn't exist.  Skipping..."
+      ]
+    }
+  }
+
+  return {
+    item: {
+      ...item,
+      category: categoryId
+    },
+    origItem,
+    logs
+  }
+}
+
+function parseNeedsMet(
+  {item, origItem, logs}: {item: any, origItem: any, logs: string[]}
+): {item: any, origItem: any, logs: string[]}  {
+  const needsMet = origItem.needsMet
+
+  // no needs met info
   if ( needsMet.items === ""
     && needsMet.people === ""
     && needsMet.months === ""
     && needsMet.type === ""
   ) {
-    return undefined
-  } else if ( needsMet.items === ""
-    && needsMet.people === ""
-    && needsMet.months === ""
-    && needsMet.type === ""
+    return {
+      item,
+      origItem,
+      logs
+    }
+  }
+  
+  if ( needsMet.items === ""
+    || needsMet.people === ""
+    || needsMet.months === ""
+    || needsMet.type === ""
   ) {
-    console.log(`  - incomplete needs data for (${product.name} // ${product.age_gender} // ${product.size_style}), ignoring...`)
-    return undefined
+    return {
+      item,
+      origItem,
+      logs: [
+        ...logs,
+        "Error: Incomplete needsMet data. Ignoring..."
+      ]
+    }
+  }
+
+  return {
+    item: {
+      ...item,
+      needsMet: {
+        items: stripAndParseInt(needsMet.items),
+        people: stripAndParseInt(needsMet.people),
+        months: stripAndParseInt(needsMet.months),
+        monthlyNeedsMetPerItem: undefined, // calculated
+        type: needsMet.type,
+        notes: needsMet.notes
+      }
+    },
+    origItem,
+    logs
   }
 }
 
-function parseSecondHand(product) {
-  const secondHand = product.secondHand
+function parseSecondHand(
+  {item, origItem, logs}: {item: any, origItem: any, logs: string[]}
+): {item: any, origItem: any, logs: string[]}  {
+  const secondHand = origItem.secondHand
 
   if ( secondHand.priceAdjustment === "N/A"
     || secondHand.priceAdjustment === ""
   ) {
     return {
-      canBeUsed: false,
-      priceAdjustment: undefined
+      item: {
+        ...item,
+        secondHand: {
+          canBeUsed: false,
+          priceAdjustment: undefined    
+        }
+      },
+      origItem,
+      logs
     }
   } else {
     return {
-      canBeUsed: true,
-      priceAdjustment: stripAndParseInt(secondHand.priceAdjustment)
+      item: {
+        ...item,
+        secondHand: {
+          canBeUsed: true,
+          priceAdjustment: stripAndParseInt(secondHand.priceAdjustment)    
+        }
+      },
+      origItem,
+      logs
     }
   }
 }
 
-function parseValue(product) {
-  const value = product.value[0]
+function parseValue(
+  {item, origItem, logs}: {item: any, origItem: any, logs: string[]}
+): {item: any, origItem: any, logs: string[]}  {
+  const value = origItem.value[0]
 
+  // no value info
   if ( value.packagePrice === ""
-    && value.packagePriceUnit === ""
+    // a lot of items have this set to "USA" by deafult, so don't consider it
+    // && value.packagePriceUnit === ""
     && value.countPerPackage === ""
     && value.source === ""
     && value.logDate === ""
   ) {
-    return []
-  } else if ( value.packagePrice === ""
-    || value.packagePriceUnit === ""
+    return {
+      item,
+      origItem,
+      logs
+    }
+  }
+  
+  if ( value.packagePrice === ""
+    // a lot of items have this set to "USA" by deafult, so don't consider it
+    // || value.packagePriceUnit === ""
     || value.countPerPackage === ""
     || value.source === ""
     || value.logDate === ""
   ) {
-    console.log(`  - incomplete price data for (${product.name} // ${product.age_gender} // ${product.size_style}), ignoring...`)
-    return []
-  } else {
-    return [{
-      packagePrice: stripAndParseFloat(value.packagePrice),
-      packagePriceUnit: value.packagePriceUnit,
-      countPerPackage: stripAndParseInt(value.countPerPackage),
-      pricePerItemUSD: undefined, // calculated
-      source: value.source,
-      logDate: value.logDate,
-      notes: value.notes
-    }]
+    return {
+      item,
+      origItem,
+      logs: [
+        ...logs,
+        "Error: Incomplete value data. Ignoring..."
+      ]
+    }
+  }
+  
+  return {
+    item: {
+      ...item,
+      value: [{
+        packagePrice: stripAndParseFloat(value.packagePrice),
+        packagePriceUnit: value.packagePriceUnit,
+        countPerPackage: stripAndParseInt(value.countPerPackage),
+        pricePerItemUSD: undefined, // calculated
+        source: value.source,
+        logDate: value.logDate,
+        notes: value.notes
+      }]
+    },
+    origItem,
+    logs
   }
 }
 
-function parseWeight(product) {
-  const weight = product.weight[0]
+function parseWeight(
+  {item, origItem, logs}: {item: any, origItem: any, logs: string[]}
+): {item: any, origItem: any, logs: string[]}  {
+  const weight = origItem.weight[0]
 
+  // no weight data
   if ( weight.packageWeight === ""
     && weight.packageWeightUnit === ""
     && weight.countPerPackage === ""
     && weight.source === ""
     && weight.logDate === ""
   ) {
-    return []
-  } else if ( weight.packageWeight === ""
+    return {
+      item,
+      origItem,
+      logs
+    }
+  }
+  
+  if ( weight.packageWeight === ""
     || weight.packageWeightUnit === ""
     || weight.countPerPackage === ""
     || weight.source === ""
     || weight.logDate === ""
   ) {
-    console.log(`  - incomplete weight data for (${product.name} // ${product.age_gender} // ${product.size_style}), ignoring...`)
-    return []
-  } else {
-    return [{
-      packageWeight: stripAndParseFloat(weight.packageWeight),
-      packageWeightUnit: weight.packageWeightUnit,
-      countPerPackage: stripAndParseInt(weight.countPerPackage),
-      itemWeightKg: undefined, // calculated
-      countPerKg: undefined, // calculated
-      source: weight.source,
-      logDate: weight.logDate,
-      notes: weight.notes
-    }]
+    return {
+      item,
+      origItem,
+      logs: [
+        ...logs,
+        "Error: Incomplete weight data. Ignoring..."
+      ]
+    }
+  }
+
+  return {
+    item: {
+      ...item,
+      weight: [{
+        packageWeight: stripAndParseFloat(weight.packageWeight),
+        packageWeightUnit: weight.packageWeightUnit,
+        countPerPackage: stripAndParseInt(weight.countPerPackage),
+        itemWeightKg: undefined, // calculated
+        countPerKg: undefined, // calculated
+        source: weight.source,
+        logDate: weight.logDate,
+        notes: weight.notes
+      }]
+    },
+    origItem,
+    logs
   }
 }
 
-function parseVolume(product) {
-  const volume = product.volume[0]
+function parseVolume(
+  {item, origItem, logs}: {item: any, origItem: any, logs: string[]}
+): {item: any, origItem: any, logs: string[]}  {
+  const volume = origItem.volume[0]
 
+  // no volume data
   if ( volume.packageVolume === ""
     && volume.packageVolumeUnit === ""
     && volume.countPerPackage === ""
     && volume.source === ""
     && volume.logDate === ""
   ) {
-    return []
-  } else if ( volume.packageVolume === ""
+    return {
+      item,
+      origItem,
+      logs
+    }
+  }
+  
+  if ( volume.packageVolume === ""
     || volume.packageVolumeUnit === ""
     || volume.countPerPackage === ""
     || volume.source === ""
     || volume.logDate === ""
   ) {
-    console.log(`  - incomplete volume data for (${product.name} // ${product.age_gender} // ${product.size_style}), ignoring...`)
-    return []
-  } else {
-    return [{
-      packageVolume: stripAndParseFloat(volume.packageVolume),
-      packageVolumeUnit: volume.packageVolumeUnit,
-      countPerPackage: stripAndParseInt(volume.countPerPackage),
-      itemVolumeCBCM: undefined, // calculated
-      countPerCBM: undefined, // calculated
-      source: volume.source,
-      logDate: volume.logDate,
-      notes: volume.notes
-    }]
+    return {
+      item,
+      origItem,
+      logs: [
+        ...logs,
+        "Error: Incomplete volume data. Ignoring..."
+      ]
+    }
+  }
+
+  return {
+    item: {
+      ...item,
+      volume: [{
+        packageVolume: stripAndParseFloat(volume.packageVolume),
+        packageVolumeUnit: volume.packageVolumeUnit,
+        countPerPackage: stripAndParseInt(volume.countPerPackage),
+        itemVolumeCBCM: undefined, // calculated
+        countPerCBM: undefined, // calculated
+        source: volume.source,
+        logDate: volume.logDate,
+        notes: volume.notes
+      }]
+    },
+    origItem,
+    logs
   }
 }
 
-/* Validate Items
- * ------------------------------------------------------ */
-function validateItems(items) {
-  return items
-}
+/* Get Item
+ * ----------------------------------------------------- */
+async function getItem(
+  {item, origItem, logs}: {item: any, origItem: any, logs: string[]}
+): Promise<{item: any, origItem: any, logs: string[]}> {
+  logs = [
+    ...logs,
+    `Log: Checking if item "${item.name} // ${item.age_gender} // ${item.size_style}" already exists.`
+  ]
 
-/* Upload New Items
- * ------------------------------------------------------ */
-function uploadNewItems(items) {
-  console.log(`    - uploading items`)
+  const query = qs.stringify({
+    filters: {
+      name: {
+        $eq: item.name,
+      },
+      age_gender: {
+        $eq: item.age_gender,
+      },
+      size_style: {
+        $eq: item.size_style
+      }
+    },
+    fields: ['id'],
+  })
 
-  return Promise.allSettled(
-    items.map((item) => {
-      return doesItemExist(item)
-        .then((exists: boolean) => {
-          if (exists) {
-            return `  - item (${item.name} // ${item.age_gender} // ${item.size_style}) already exists. skipping it...`
-          } else {
-            return uploadItem(item)
-          }
-        })
-    })
-  )
+  const response = await fetch(`${STRAPI_ENV.URL}/items?${query}`, {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+        'Authorization': `Bearer ${STRAPI_ENV.KEY}`
+    },
+  })
+  const body = await response.json()
+
+  if (!response.ok) {
+    throw {
+      item,
+      origItem,
+      logs: [
+        ...logs,
+        `Error: Failed to get item. HttpStatus: ${response.status} - ${response.statusText}`,
+        JSON.stringify(body)
+      ]
+    }
+  }
+
+  if (body.data.length > 1) {
+    throw {
+      item,
+      origItem,
+      logs: [
+        ...logs,
+        `Error: Found too many matching items (${body.data.length}). Skipping...`
+      ]
+    }
+  }
+
+  if (body.data.length === 1) {
+    throw {
+      item: {
+        ...item,
+        id: body.data[0].id
+      },
+      origItem,
+      logs: [
+        ...logs,
+        "Log: Found existing item. Skipping..."
+      ]
+    }
+  }
+
+  return {
+    item: body.data[0],
+    origItem,
+    logs: [
+      ...logs,
+      "Success: Confirmed item does not exist."
+    ]
+  }
 }
 
 /* Upload Item
  * ------------------------------------------------------ */
-function uploadItem(item) {
-  return fetch(`${STRAPI_ENV.URL}/items`, {
+async function uploadItem(
+  {item, origItem, logs}: {item: any, origItem: any, logs: string[]}
+): Promise<{item: any, origItem: any, logs: string[]}> {
+  logs = [
+    ...logs,
+    `Log: Creating item "${item.name} // ${item.age_gender} // ${item.size_style}".`
+  ]
+
+  const response = await fetch(`${STRAPI_ENV.URL}/items`, {
     method: "POST",
     headers: {
         "Content-Type": "application/json",
@@ -229,12 +480,26 @@ function uploadItem(item) {
     },
     body: JSON.stringify({data: item})
   })
-    .then(async (response) => {
-      if (response.ok) {
-        const result = await response.json()
-        return `  - created item (${item.name} // ${item.age_gender} // ${item.size_style}). id=${result.data.id}`
-      } else {
-//        throw new Error(`  - failed to create item (${item.name} // ${item.age_gender} // ${item.size_style}). HttpError: ${response.status} - ${response.statusText}`)
-      }
-    })
+  const body = await response.json()
+
+  if (!response.ok) {
+    throw {
+      item,
+      origItem,
+      logs: [
+        ...logs,
+        `Error: Failed to create item. HttpStatus: ${response.status} - ${response.statusText}`,
+        JSON.stringify(body)
+      ]
+    }
+  }
+
+  return {
+    item: body.data,
+    origItem,
+    logs: [
+      ...logs,
+      "Success: Created item."
+    ]
+  }
 }
