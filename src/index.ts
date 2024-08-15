46efc0c04b24c7c11784b26cff7dd35f2f0b6028
D Dixon <madcalf@users.noreply.github.com>
2024-08-15 01:45:10 -0700

    WIP setting public permissions via bootstrap function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
 
 export default {
   /**
@@ -16,5 +16,62 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  async bootstrap({ strapi }: { strapi: Core.Strapi }) {
+    // const allPermissions = await strapi
+    //   .query("plugin::users-permissions.permission")
+    //   .findMany({
+    //     populate: true,
+    //     limit: 10,
+    //     where: { role: { type: "public" } },
+    //   });
+
+    // strapi.log.info(`allPermissions: ${JSON.stringify(allPermissions)}`);
+
+    async function createPublicPermissions() {
+      const newPermissions = {
+        geo: {
+          country: ["find", "findOne", "create", "update", "delete"],
+          region: ["find", "findOne", "create", "update", "delete"],
+          subregion: ["find", "findOne", "create", "update", "delete"],
+        },
+        product: {
+          category: ["find", "findOne", "create", "update", "delete"],
+          item: ["find", "findOne", "create", "update", "delete"],
+        },
+      };
+
+      // get the public role id
+      const publicRole = await strapi
+        .query("plugin::users-permissions.role")
+        .findOne({
+          where: { type: "public" },
+        });
+
+      // set the permissions
+      const promises = [];
+      Object.keys(newPermissions).map((group) => {
+        Object.keys(newPermissions[group]).map((endpoint) => {
+          const actions = newPermissions[group][endpoint];
+
+          const permissionsToCreate = actions.map((action) => {
+            strapi.log.info(
+              `creating public role for '${group}.${endpoint}.${action}'`,
+            );
+
+            return strapi.query("plugin::users-permissions.permission").create({
+              data: {
+                action: `api::${group}.${endpoint}.${action}`,
+                role: publicRole.id,
+              },
+            });
+          });
+
+          promises.push(permissionsToCreate);
+        });
+        Promise.all(promises);
+      });
+    }
+
+    await createPublicPermissions();
+  },
 };
