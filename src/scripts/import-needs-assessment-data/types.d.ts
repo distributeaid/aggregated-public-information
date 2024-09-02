a59f045eb1800fd5ee9e48dd89248766d8aac76b
Laura Z. <llzerr@hotmail.com>
2024-09-02 21:18:25 +0000

    Fix formatting issues

--- src/scripts/import-needs-assessment-data/types.d.ts
+++ src/scripts/import-needs-assessment-data/types.d.ts
@@ -1,57 +1,56 @@
 export enum UploadWorkflowStatus {
-    PROCESSING = "PROCESSING",
-    ORIGINAL_DATA_INVALID = "ORIGINAL_DATA_INVALID",
-    DUPLICATE_CHECK_ERROR = "DUPLICATE_CHECK_ERROR",
-    ALREADY_EXISTS = "ALREADY_EXISTS",
-    UPLOAD_ERROR = "UPLOAD_ERROR",
-    SUCCESS = "SUCCESS",
-    OTHER = "OTHER",
+  PROCESSING = "PROCESSING",
+  ORIGINAL_DATA_INVALID = "ORIGINAL_DATA_INVALID",
+  DUPLICATE_CHECK_ERROR = "DUPLICATE_CHECK_ERROR",
+  ALREADY_EXISTS = "ALREADY_EXISTS",
+  UPLOAD_ERROR = "UPLOAD_ERROR",
+  SUCCESS = "SUCCESS",
+  OTHER = "OTHER",
 }
 
 export type UploadWorkflow<T> = {
-    data: T;
-    orig: string | T;   // Union type allowing either a csv string or parsed JSON object
-    status: UploadWorkflowStatus;
-    logs: string[];
+  data: T;
+  orig: string | T; // Union type allowing either a csv string or parsed JSON object
+  status: UploadWorkflowStatus;
+  logs: string[];
 };
 
 export type UploadWorkflowResults<T> = {
-    [Property in keyof UploadWorkflowStatus]: T[];
+  [Property in keyof UploadWorkflowStatus]: T[];
 };
 
-export type RegionUploadWorkflow = UploadWorkflow<Region>
+export type RegionUploadWorkflow = UploadWorkflow<Region>;
 export type RegionUploadWorkflowResults = UploadWorkflowResults<Region>;
-export type SubregionUploadWorkflow = UploadWorkflow<Subregion>
+export type SubregionUploadWorkflow = UploadWorkflow<Subregion>;
 export type SubregionUploadWorkflowResults = UploadWorkflowResults<Subregion>;
 
-
 export type Region = {
-    region?: string;
+  region?: string;
 };
 
 export type Subregion = {
-    subregion?: string;
+  subregion?: string;
 };
 
 export type NeedAssessment = {
+  id?: string;
+  survey?: {
     id?: string;
-    survey?: {
-        id?: string;
-        year?: string;
-        quarter?: string;
-        url?: string;
-        format?: string;
-    };
-    place?: {
-        region?: string;
-        subregion?: string;
-    };
-    product?: {
-        category?: string;
-        item?: string;
-        ageGender?: string;
-        sizeStyle?: string;
-        unit?: string;
-    };
-    need?: string;
+    year?: string;
+    quarter?: string;
+    url?: string;
+    format?: string;
+  };
+  place?: {
+    region?: string;
+    subregion?: string;
+  };
+  product?: {
+    category?: string;
+    item?: string;
+    ageGender?: string;
+    sizeStyle?: string;
+    unit?: string;
+  };
+  need?: string;
 };
