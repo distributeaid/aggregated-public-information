4f7cb96d2b9fc6c5e7a4ba69ac384394e4447530
Cora Sutton <cora@sutton.me>
2024-09-05 11:47:35 -0500

    Update the setup script to be quote-happy

--- setup.bash
+++ setup.bash
@@ -5,14 +5,14 @@ EXAMPLE_ENV_FILE=".env.example"
 ENV_FILE=".env"
 
 # Create a new .env file
-cp ${EXAMPLE_ENV_FILE} ${ENV_FILE}
+cp "$EXAMPLE_ENV_FILE" "$ENV_FILE"
 echo "Creating new .env file"
 
 # Function to generate a valid key
 generate_valid_key() {
   while true; do
     # Generate a base64 key
-    KEY=$(openssl rand -base64 16)
+    KEY="$(openssl rand -base64 16)"
 
     # Check if the key contains only allowed characters (alphanumeric and '=' sign)
     if echo "$KEY" | grep -qE '^[a-zA-Z0-9=]+$'; then
@@ -25,7 +25,7 @@ generate_valid_key() {
 # Loop to generate 8 keys and replace them in the .env file
 for i in $(seq 1 8); do
   # Generate a valid key
-  VALID_KEY=$(generate_valid_key)
+  VALID_KEY="$(generate_valid_key)"
 
   # Ensure the placeholder exists in the .env file
   if ! grep -q "key${i}==" "$ENV_FILE"; then
