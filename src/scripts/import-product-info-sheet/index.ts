55be41f3f51b58be96b78fde01da5a7aa063ce7b
eli <e.feasley@gmail.com>
2024-09-08 20:18:40 -0400

    Product script import refactor and create demo data
    Changes:
    Update .env.example to import either demo data that imports without errors or exported data (if specified)
    Added products.csv, a 'clean' export of the products data that should import without errors
    Added -v to print optionally print details of all import errors
    Refactored index.ts to import specified data rathher than with a hardcoded filename.
    Updated and clarified instructions in index.ts
    
    I identified a few bugs which I will open as issues
    
    1. The canonical name for each product does not include the category.
    As an example, Kitchen, Bottle Brush and Baby, Bottle Brush show up as duplicates.
    This issue should be resolved and a test added.
    
    Next steps include
    1. incorporating any other export scripts (not yet merged to saga) with this same framework of demo error-free data to populate the data model locally.
    2. adding a smaller amount of data to the set-up/tear-down for the test framework
    3. Returning a gentler info than the warning of object already exists when someone tries to incorporate data that is identical to an existing entry (as opposed to importing data that would overwrite previous data)
    4. Creating a dry-run or simulating one as closely as possible that outputs all prospective errors without updating data until they have been resolved.
    5. Auto-normalizing currency (e.g. USd to USD)

--- src/scripts/import-product-info-sheet/index.ts
+++ src/scripts/import-product-info-sheet/index.ts
@@ -1,65 +1,47 @@
 import "dotenv/config";
-import csvToJson from "csvtojson";
 
 import addCategories from "./add-categories";
 import addItems from "./add-items";
+import loadProducts from "./load-products";
 
 /* How to run this script:
      1. Setup your scripts `.env` file (copy / modify `.env.example`)
+        This is not the same as the .env file you set up to build strapi - there is a separate .env file for scripts.
+        It's located in the scripts directory.
+        `cp src/scripts/.env.example src/scripts/.env`
+        Remeber to generate and add a Strapi API key to the .env file.
      2. Export the Product Info sheet to a .csv and put it in the
-        `src/scripts/upload-product-info-sheet/` directory.
-     3. Delete the first 3 lines (headers).
+        `src/scripts/import-product-info-sheet/` directory.
+        Save it with the name products.csv
+     3. Delete the first 4 lines (headers).
      4. Run the script from the project root directory:
         `yarn script:import-product-info-sheet`
+     5. To run in verbose mode, add -v
+        To just load the demo data, set the PRODUCTS_EXPORT_PATH to an empty string in the .env file.
+
+    // TODO: If the item already exists but with different data, that's an error (unlike if it already exists with identical data).
+      Implement a deep comparison function to check for this.
+    // TODO: Replace NAN with 0 in the product data? Or reject?
+    // TODO: Vlear out the existing data with an existing command and a confirmation prompt.
+    // Removing the whome temp data is annoying bc you need to make a new account and configure a new api key.
 */
 
-const products = await csvToJson({
-  noheader: true,
-  headers: [
-    "category.name",
-    "name",
-    "age_gender",
-    "size_style",
+function configureEnvironment() {
+  // Parse command-line arguments
+  const args = process.argv.slice(2);
+  process.env.VERBOSE = args.includes("-v") ? "true" : "";
 
-    "value.0.packagePriceUnit",
-    "value.0.packagePrice",
-    "value.0.countPerPackage",
-    "value.0.pricePerItemUSD",
-    "value.0.source",
-    "value.0.logDate",
-    "value.0.notes",
+  // If the environmental variables aren't set, remind the user where to set them.
+  if (!process.env.STRAPI_URL || !process.env.STRAPI_API_KEY) {
+    console.error("Please set STRAPI_URL and STRAPI_API_KEY in the .env file in the scripts directory.");
+    process.exit(1);
+  }
+}
 
-    "secondHand.priceAdjustment",
 
-    "weight.0.packageWeight",
-    "weight.0.packageWeightUnit",
-    "weight.0.countPerPackage",
-    "weight.0.itemWeightKg",
-    "weight.0.countPerKg",
-    "weight.0.source",
-    "weight.0.logDate",
-    "weight.0.notes",
-
-    "volume.0.packageVolume",
-    "volume.0.packageVolumeUnit",
-    "volume.0.countPerPackage",
-    "volume.0.itemVolumeCBCM",
-    "volume.0.countPerCBM",
-    "volume.0.source",
-    "volume.0.logDate",
-    "volume.0.notes",
-
-    "needsMet.items",
-    "needsMet.people",
-    "needsMet.months",
-    "needsMet.monthlyNeedsMetPerItem",
-    "needsMet.type",
-    "needsMet.notes",
-
-    "packSize",
-    "uuid",
-  ],
-}).fromFile("src/scripts/import-product-info-sheet/products.csv");
-
-await addCategories(products);
-await addItems(products);
+(async () => {
+  configureEnvironment();
+  const productData = await loadProducts();
+  await addCategories(productData);
+  await addItems(productData);
+})();
