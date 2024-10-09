e44dbf26cfb5079048cc59233aa46cabf0481374
Laura Z. <llzerr@hotmail.com>
2024-10-09 17:46:29 +0000

    Create a file to add surveys data

---
+++ src/scripts/import-needs-assessment-data/add-surveys.ts
@@ -0,0 +1,23 @@
+import { STRAPI_ENV } from "../strapi-env";
+import {
+  Survey,
+  NeedAssessment,
+  SurveyUploadWorkflow,
+  UploadWorkflowStatus,
+  SurveyUploadWorkflowResults,
+} from "./types.d";
+
+/*  Add Surveys from Needs Assessment Data
+ * ------------------------------------------------------- */
+
+/*  Consolidate Surveys
+ * ------------------------------------------------------- */
+
+/*  Parse Surveys
+ * ------------------------------------------------------- */
+
+/*  Get Surveys
+ * ------------------------------------------------------- */
+
+/*  Upload Surveys
+ * ------------------------------------------------------- */
