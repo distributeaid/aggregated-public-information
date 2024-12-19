217678f6355f1c84a968c5e9a99cfbe0f953b2a4
Laura Z. <llzerr@hotmail.com>
2024-12-19 18:49:15 +0000

    Fix format issue

--- src/scripts/import-needs-assessment-data/add-items.ts
+++ src/scripts/import-needs-assessment-data/add-items.ts
@@ -42,7 +42,11 @@ export function consolidateProductsByCategory(
 
 /*  Parse Products
  * ------------------------------------------------------- */
-export function parseProducts(data: Product[], orig, _status): ProductUploadWorkflow {
+export function parseProducts(
+  data: Product[],
+  orig,
+  _status,
+): ProductUploadWorkflow {
   const logs = [];
   logs.push(`Parsing products ...`);
 
@@ -53,24 +57,23 @@ export function parseProducts(data: Product[], orig, _status): ProductUploadWork
     data.forEach((item: Product) => {
       logs.push(`Parsing product: ${item.item}`);
 
-    if (!item.category || !item.item || !item.unit) {
-      problematicItems.push(item);
-    } else {
-      const processedProduct: Product = {
-        ...item,
-      };
-      parsedData.push(processedProduct);
-    }
-
-  });
+      if (!item.category || !item.item || !item.unit) {
+        problematicItems.push(item);
+      } else {
+        const processedProduct: Product = {
+          ...item,
+        };
+        parsedData.push(processedProduct);
+      }
+    });
   } else {
     console.log("Data property is not an array");
   }
 
   const hasProblematicItems = problematicItems.length > 0;
   const updatedStatus = hasProblematicItems
-  ? UploadWorkflowStatus.PROCESSING
-  : UploadWorkflowStatus.SUCCESS
+    ? UploadWorkflowStatus.PROCESSING
+    : UploadWorkflowStatus.SUCCESS;
 
   logs.push(
     `Workflow completed. Status: ${problematicItems.length} product items were problematic: ${JSON.stringify(problematicItems, null, 2).replace(/\\n|"/g, "")}`,
