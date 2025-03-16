a56c687647883931bbc234ea3752c107d44eacaf
Laura Z. <llzerr@hotmail.com>
2025-03-16 17:15:33 +0000

    Create the add needs file with required imports

---
+++ src/scripts/import-needs-assessment-data/add-needs.ts
@@ -0,0 +1,11 @@
+// import qs from "qs";
+// import { STRAPI_ENV } from "../strapi-env";
+// import { UploadWorkflowStatus } from "../statusCodes";
+// import {
+//   Need,
+//   NeedAssessment,
+//   NeedUploadWorkflow,
+//   NeedUploadWorkflowResults,
+// } from "./types.d";
+
+// Note: Uncomment the required imports during implementation in the code.
