5503d63ec3eb3b1463d42d68ee5253c7e39326fa
Miguel Johnson <miguelbjohnson@gmail.com>
2025-07-17 20:33:20 -0400

    fix: format fix

--- src/functions/content-types/reporting/types.ts
+++ src/functions/content-types/reporting/types.ts
@@ -3,17 +3,17 @@ export type CargoItem = {
 };
 
 export type CargoPackageUnit =
-    "Banana Box"
-    | "Box"
-    | "Large Box"
-    | "Euro Pallet"
-    | "Pallet"
-    | "Bag"
-    | "Medium Bag"
-    | "Large Bag"
-    | "Bulk Bag"
-    | "Item"
-    | "Single Item";
+  | "Banana Box"
+  | "Box"
+  | "Large Box"
+  | "Euro Pallet"
+  | "Pallet"
+  | "Bag"
+  | "Medium Bag"
+  | "Large Bag"
+  | "Bulk Bag"
+  | "Item"
+  | "Single Item";
 
 export type CargoPackage = {
   packageUnit: CargoPackageUnit;
