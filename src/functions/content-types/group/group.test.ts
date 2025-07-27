00c60eb606ccfa0cbdfdebc13c364fdc171810a4
Kellan Heth <kellansh@gmail.com>
2025-07-27 16:35:59 -0400

    Break test into three parts

--- src/functions/content-types/group/group.test.ts
+++ src/functions/content-types/group/group.test.ts
@@ -1,31 +1,45 @@
 import { processGroup } from "./group";
 
 describe("processGroup", () => {
-  const input1 = "670 WORDS ";
-  const input2 = "ɐ  wORD";
-  const input3 = "   ɏes";
-
-  const output1 = "670 Words";
-  const output2 = "Word";
-  const output3 = "Ɏes";
-
-  it("strips names correctly", () => {
-    const testData = {
-      shortName: input1,
-      groupName: input2,
-      groupConvertFrom: input3,
+  it("strips shortName correctly", () => {
+    const hasShortName = {
+      shortName: "670 WORDS ",
     };
 
-    const expectedOutput = {
-      shortName: input1,
-      groupName: input2,
-      groupConvertFrom: input3,
-      shortNameStripped: output1,
-      groupNameStripped: output2,
-      groupConvertFromStripped: output3,
+    const hasShortNameStripped = {
+      shortName: hasShortName.shortName,
+      shortNameStripped: "670 Words",
     };
 
-    const testDataProcessed = processGroup(testData);
-    expect(testDataProcessed).toEqual(expectedOutput);
+    const stripShortName = processGroup(hasShortName);
+    expect(stripShortName).toEqual(hasShortNameStripped);
+  });
+
+  it("strips groupName correctly", () => {
+    const hasGroupName = {
+      groupName: "ɐ  wORD",
+    };
+
+    const hasGroupNameStripped = {
+      groupName: hasGroupName.groupName,
+      groupNameStripped: "Word",
+    };
+
+    const stripGroupName = processGroup(hasGroupName);
+    expect(stripGroupName).toEqual(hasGroupNameStripped);
+  });
+
+  it("strips groupConvertFrom correctly", () => {
+    const hasGroupConvertFrom = {
+      groupConvertFrom: "   ɏes",
+    };
+
+    const hasGroupConvertFromStripped = {
+      groupConvertFrom: hasGroupConvertFrom.groupConvertFrom,
+      groupConvertFromStripped: "Ɏes",
+    };
+
+    const stripGroupConvertFrom = processGroup(hasGroupConvertFrom);
+    expect(stripGroupConvertFrom).toEqual(hasGroupConvertFromStripped);
   });
 });
