11f441a4167d0f9374e2f7e62a3eb40281e28947
laura_z <llzerr@hotmail.com>
2026-07-04 17:47:05 -0600

    Format fix

--- src/api/response/routes/fundraiser.ts
+++ src/api/response/routes/fundraiser.ts
@@ -2,6 +2,6 @@
  * response router
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreRouter('api::response.fundraiser');
+export default factories.createCoreRouter("api::response.fundraiser");
