568653a52a3ed95ad01eb565df54a525c9b21b8f
laura_z <llzerr@hotmail.com>
2026-07-04 17:47:05 -0600

    Format fix

--- src/api/response/controllers/fundraiser.ts
+++ src/api/response/controllers/fundraiser.ts
@@ -2,6 +2,6 @@
  * response controller
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::response.fundraiser');
+export default factories.createCoreController("api::response.fundraiser");
